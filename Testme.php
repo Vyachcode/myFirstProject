5b3251a50ced31baa6dad7cec6fd466b338e8bfa
vyachcode <vyacheslav.inv@ya.ru>
2012-12-16 16:07:01 +0600

    Первый коммит, если получится.

---
+++ Testme.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Vyacheskav
+ * Date: 16.12.12
+ * Time: 16:04
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo "Test 1";
+
+?>
